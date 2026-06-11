56147b1faeab2ba68803143573451c95a2fc91d4
Félix Gray-Sylvain <felixgs2005@gmail.com>
2026-06-11 14:38:56 -0400

    img

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Outlet, NavLink, Link, useLocation } from 'react-router-dom'
-import logoGradient from '../assets/logo-gradient.webp'
+import logoGradient from './assets/logo-gradient.webp'
 import ScrollToTop from './ScrollToTop'
 import styles from './Layout.module.css'
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import cellphone from '../assets/cellphone.webp'
+import cellphone from './assets/cellphone.webp'
 import styles from './Home.module.css'
 
 const values = [
