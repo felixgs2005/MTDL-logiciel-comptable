65ccd00439254aea89e4d7602ecd709bff7411c2
Félix Gray-Sylvain <felixgs2005@gmail.com>
2026-06-11 15:59:34 -0400

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Services from './pages/Services'
@@ -22,7 +23,7 @@ export default function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<><ScrollToTopOnNav /><Home /></>} />
         <Route path="services" element={<><ScrollToTopOnNav /><Services /></>} />
-        <Route path="avis" element={<><ScrollToTopOnNav />Avis  /></>} />
+        <Route path="avis" element={<><ScrollToTopOnNav /><Avis  /></>} />
         <Route path="saviez-vous-que" element={<><ScrollToTopOnNav /><SaviezVousQue /></>} />
         <Route path="a-propos" element={<><ScrollToTopOnNav /><APropos /></>} />
         <Route path="tenue-de-livres" element={<><ScrollToTopOnNav /><TenueLivres /></>} />
